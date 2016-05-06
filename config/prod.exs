85b63aaf6feac00a1ac361dd6e7f599b38849c03
Jay Hayes <ur@iamvery.com>
2016-05-06 15:00:27 -0500

    Get secret key base from environment

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,8 @@ config :swolen, Swolen.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "swolen-mud.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -61,6 +62,3 @@ config :logger, level: :info
 #
 #     config :swolen, Swolen.Endpoint, root: "."
 
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
